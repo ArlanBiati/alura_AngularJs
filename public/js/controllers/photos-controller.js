bad44d7d21431b3b30c77410da6c1af4b6d26b2b
Arlan Biati <arlangustavo@hotmail.com>
2020-11-09 22:20:52 -0300

    Chore: iniciando aplicação

---
+++ public/js/controllers/photos-controller.js
@@ -0,0 +1,6 @@
+angular.module('alurapic').controller('PhotosController', function ($scope) {
+  $scope.photo = {
+    title: "Leão",
+    url: "http://www.fundosanimais.com/Minis/leoes.jpg"
+  }
+})
